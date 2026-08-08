e5f69f22e7047fb902da925b5372a5aee0e9503d
rsrc191 <67526005+rsrc191@users.noreply.github.com>
2026-08-08 12:10:16 -0700

    Change email and password to hardcoded values
    Replaced environment variable retrieval with hardcoded email and password.

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import smtplib
 import os
 
 # import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+MY_EMAIL = os.environ.get("ramkumarrs2018@gmail.com")
+MY_PASSWORD = os.environ.get("kwgtoblbaqscuaml")
 
 today = datetime.now()
 today_tuple = (today.month, today.day)
